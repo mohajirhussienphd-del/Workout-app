561b08d424f06d817242618950b0fa5bedcfea10
mohajirhussienphd-del <mohajir.hussien.phd@gmail.com>
2026-08-17 17:19:56 +0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,9 @@
+const CACHE = 'ironwill-v1';
+self.addEventListener('install', (e) => {
+  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(['./', './index.html', './manifest.json'])));
+});
+self.addEventListener('activate', (e) => e.waitUntil(self.clients.claim()));
+self.addEventListener('fetch', (e) => {
+  e.respondWith(caches.match(e.request).then((r) => r || fetch(e.request)));
+});
